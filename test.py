f7cbbf05552e1359850165a317d5deeec9f2c2fd
Rob Cobb <rwcobbjr@gmail.com>
2022-10-15 18:18:34 +0000

    fix module cleanup

--- test.py
+++ test.py
@@ -7,8 +7,6 @@ import io
 class Test(TestCase):
     def test_first_ten_lines(self):
         with patch('sys.stdout', new=io.StringIO()) as mock_stdout:
-            import main
-
             expected_lines = """1
 2
 3 is a multiple of 3
@@ -20,11 +18,13 @@ class Test(TestCase):
 9 is a multiple of 3
 10 is a multiple of 5""".split("\n")
 
-            actual_lines = mock_stdout.getvalue().split("\n")
-            for i, expected in enumerate(expected_lines):
-                self.assertEqual(expected, actual_lines[i])
-
-            sys.modules.pop('main')
+            import main
+            try:
+                actual_lines = mock_stdout.getvalue().split("\n")
+                for i, expected in enumerate(expected_lines):
+                    self.assertEqual(expected, actual_lines[i])
+            finally:
+                sys.modules.pop('main')
 
 if __name__ == '__main__':
     unittest.main()
